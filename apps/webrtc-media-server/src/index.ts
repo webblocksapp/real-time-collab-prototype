25e2f76ebe6b07e6c59c244f4744a38cbcaa97b0
webblocksapp <mauriver21@gmail.com>
2024-09-14 17:07:07 -0500

    Refactor streaming endpoints.

--- apps/webrtc-media-server/src/index.ts
+++ apps/webrtc-media-server/src/index.ts
@@ -4,6 +4,7 @@ import net from 'net';
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 import 'log-timestamp';
 import dgram from 'dgram';
+import { PassThrough } from 'stream';
 
 const app = express();
 app.use(express.static(__dirname + '/'));
@@ -14,12 +15,16 @@ const port = 9001; // change port number if required
 let gstMuxer: ChildProcessWithoutNullStreams | undefined;
 
 // send the HTML page that holds the video tag
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/tcp', (_: Request, res: Response) => {
+  res.sendFile(__dirname + '/tcp.html');
+});
+
+app.get('/udp', (_: Request, res: Response) => {
+  res.sendFile(__dirname + '/udp.html');
 });
 
 // stop the connection
-app.post('/stop', (req: Request, res: Response) => {
+app.post('/stop', (_: Request, res: Response) => {
   console.log('Connection closed using /stop endpoint.');
 
   if (gstMuxer) {
@@ -31,56 +36,75 @@ app.post('/stop', (req: Request, res: Response) => {
 });
 
 // send the video stream
-// app.get('/stream', (req: Request, res: Response) => {
-//   res.writeHead(200, {
-//     'Content-Type': 'video/webm',
-//   });
-
-//   const tcpServer = net.createServer((socket) => {
-//     socket.on('data', (data) => {
-//       res.write(data);
-//     });
-
-//     socket.on('close', (hadError) => {
-//       console.log('Socket closed.');
-//       res.end();
-//     });
-//   });
-
-//   tcpServer.maxConnections = 1;
-
-//   tcpServer.listen(() => {
-//     console.log('Connection started.');
-
-//     if (!gstMuxer) {
-//       console.log('inside gstMuxer == undefined');
-//       const cmd = 'gst-launch-1.0';
-//       const args = getGstPipelineArguments(tcpServer);
-//       console.log(`${cmd} ${args.join(' ')}`);
-//       gstMuxer = spawn(cmd, args);
-
-//       gstMuxer.stderr.on('data', onSpawnError);
-//       gstMuxer.on('exit', onSpawnExit);
-//     } else {
-//       console.log('New GST pipeline rejected because gstMuxer != undefined.');
-//     }
-//   });
-// });
-
-app.get('/stream', (req: Request, res: Response) => {
+app.get('/stream-tcp', (req: Request, res: Response) => {
   res.writeHead(200, {
     'Content-Type': 'video/webm',
   });
 
+  const tcpServer = net.createServer((socket) => {
+    socket.on('data', (data) => {
+      res.write(data);
+    });
+
+    socket.on('close', (hadError) => {
+      console.log('Socket closed.');
+      res.end();
+    });
+  });
+
+  tcpServer.maxConnections = 1;
+
+  tcpServer.listen(() => {
+    console.log('Connection started.');
+
+    if (!gstMuxer) {
+      console.log('inside gstMuxer == undefined');
+      const cmd = 'gst-launch-1.0';
+      const args = [
+        'avfvideosrc',
+        'device-index=0',
+        '!',
+        'video/x-raw,width=1920,height=1080',
+        '!',
+        'x264enc',
+        'bitrate=2000',
+        'key-int-max=10',
+        '!',
+        'queue',
+        '!',
+        'mp4mux',
+        'fragment-duration=1',
+        '!',
+        'tcpclientsink',
+        'host=localhost',
+        //@ts-ignore
+        'port=' + (tcpServer.address() as net.AddressInfo).port,
+      ];
+      console.log(`${cmd} ${args.join(' ')}`);
+      gstMuxer = spawn(cmd, args);
+      gstMuxer.stderr.on('data', onSpawnError);
+      gstMuxer.on('exit', onSpawnExit);
+    } else {
+      console.log('New GST pipeline rejected because gstMuxer != undefined.');
+    }
+  });
+});
+
+app.get('/stream-udp', (req: Request, res: Response) => {
   const udpServer = dgram.createSocket('udp4');
 
+  // Crear un PassThrough stream para redirigir los datos UDP al cliente HTTP
+  const passthrough = new PassThrough();
+  passthrough.pipe(res);
+
+  // Enviar los datos UDP al PassThrough stream
   udpServer.on('message', (message) => {
-    res.write(message);
+    passthrough.write(message);
   });
 
   udpServer.on('close', () => {
     console.log('UDP server closed.');
-    res.end();
+    passthrough.end();
   });
 
   udpServer.bind(5000, () => {
@@ -89,7 +113,23 @@ app.get('/stream', (req: Request, res: Response) => {
     if (!gstMuxer) {
       console.log('inside gstMuxer == undefined');
       const cmd = 'gst-launch-1.0';
-      const args = getGstPipelineArguments({ port: udpServer.address().port });
+      const args = [
+        'avfvideosrc',
+        'device-index=0',
+        '!',
+        'video/x-raw,width=1920,height=1080',
+        '!',
+        'x264enc',
+        'tune=zerolatency',
+        'key-int-max=1',
+        '!',
+        'rtph264pay',
+        '!',
+        'udpsink',
+        'host=0.0.0.0',
+        //@ts-ignore
+        'port=' + udpServer.address().port,
+      ];
       console.log(`${cmd} ${args.join(' ')}`);
       gstMuxer = spawn(cmd, args);
 
@@ -118,50 +158,4 @@ function onSpawnExit(code: number | null) {
   if (code !== null) {
     console.log(`GStreamer error, exit code ${code}`);
   }
-}
-
-function getGstPipelineArguments(params: { port: number }): string[] {
-  // Replace 'videotestsrc', 'pattern=ball' with camera source in below GStreamer pipeline arguments.
-
-  const CONFIG_1 = [
-    'avfvideosrc',
-    'device-index=0',
-    '!',
-    'video/x-raw,width=1920,height=1080',
-    '!',
-    'x264enc',
-    'bitrate=2000',
-    'key-int-max=10',
-    '!',
-    'queue',
-    '!',
-    'mp4mux',
-    'fragment-duration=1',
-    '!',
-    'tcpclientsink',
-    'host=localhost',
-    //@ts-ignore
-    'port=' + params.port,
-  ];
-
-  const CONFIG_2 = [
-    'avfvideosrc',
-    'device-index=0',
-    '!',
-    'video/x-raw,width=1920,height=1080',
-    '!',
-    'x264enc',
-    'tune=zerolatency',
-    'key-int-max=1',
-    '!',
-    'rtph264pay',
-    '!',
-    'udpsink',
-    'host=0.0.0.0',
-    //@ts-ignore
-    'port=' + params.port,
-  ];
-
-  const args: string[] = CONFIG_2;
-  return args;
 }
