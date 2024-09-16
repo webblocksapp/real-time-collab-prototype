9fee32f0ab54834acb53d1f16817ca9a1c0bfba9
webblocksapp <mauriver21@gmail.com>
2024-09-16 10:50:11 -0500

    WIP worker

--- apps/webrtc-media-server/src/index.ts
+++ apps/webrtc-media-server/src/index.ts
@@ -3,45 +3,91 @@ import http from 'http';
 import cors from 'cors';
 import { Server } from 'socket.io';
 import { Http2Server } from 'http2';
+import * as mediasoup from 'mediasoup';
 
-const app = express();
-const PORT = 3000;
-const ALLOWED_ORIGINS = ['http://localhost:5173'];
+const main = async () => {
+  const app = express();
+  const PORT = 3000;
+  const ALLOWED_ORIGINS = ['http://localhost:5173'];
 
-const server = http.createServer(app);
-const io = new Server(server as unknown as Http2Server, {
-  cors: {
-    origin: (origin, callback) => {
-      if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+  const server = http.createServer(app);
+  const io = new Server(server as unknown as Http2Server, {
+    cors: {
+      origin: (origin, callback) => {
+        if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST'],
     },
-    methods: ['GET', 'POST'],
-  },
-});
+  });
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+    })
+  );
+
+  const peers = io.of('/mediasoup');
+
+  let worker: mediasoup.types.Worker<mediasoup.types.AppData>;
+  let router;
+
+  const createWorker = async () => {
+    worker = await mediasoup.createWorker({
+      rtcMinPort: 2000,
+      rtcMaxPort: 2020,
+    });
+    console.log(`worker pid ${worker.pid}`);
+    worker.on('died', () => {
+      console.error('mediasoup worker has died');
+      setTimeout(() => process.exit(1), 2000);
+    });
+    return worker;
+  };
+
+  worker = await createWorker();
+
+  const mediaCodecs: mediasoup.types.RtpCodecCapability[] = [
+    {
+      kind: 'audio',
+      mimeType: 'audio/opus',
+      clockRate: 48000,
+      channels: 2,
     },
-    credentials: true,
-  })
-);
+    {
+      kind: 'video',
+      mimeType: 'video/VP8',
+      clockRate: 90000,
+      parameters: {
+        'x-google-start-bitrate': 1000,
+      },
+    },
+  ];
 
-const peers = io.of('/mediasoup');
+  peers.on('connection', async (socket) => {
+    console.log(socket.id);
 
-peers.on('connection', (socket) => {
-  console.log(socket.id);
-  socket.emit('connection-success', { socketId: socket.id });
-});
+    socket.emit('connection-success', { socketId: socket.id });
+    socket.on('disconnect', () => {
+      console.log('peer disconnected');
+    });
 
-server.listen(PORT, () => {
-  console.log('Application running on port ' + PORT);
-});
+    router = await worker.createRouter({ mediaCodecs });
+  });
+
+  server.listen(PORT, () => {
+    console.log('Application running on port ' + PORT);
+  });
+};
+
+main();
